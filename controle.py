60bd4c02ad4a95cca12ea6007afcf0d5d43110c0
NLMT-UFLA <43851045+NLMT-UFLA@users.noreply.github.com>
2018-10-16 17:57:30 -0300

    Delete controle.py

--- controle.py
+++
@@ -1,9 +0,0 @@
-from carro import Carro
-carro = Carro()
-
-string = [a,b]
-    def control(string): #Usar um dicionario para executar comandos
-        return{
-            'a':carro._freio,
-            'b':carro._motor,
-        }[string]
