a186362cc64137356bf9cc54545b00bc35ace36a
NLMT-UFLA <43851045+NLMT-UFLA@users.noreply.github.com>
2018-10-16 17:57:41 -0300

    Add files via upload

--- codigos python/controle.py
+++ codigos python/controle.py
@@ -1,13 +1,9 @@
 from carro import Carro
-from teclado import Comandos
-from receptor import Receptor
-from freio import Freio
-class Controle:
-    def __init__(self):
-        self._freio = Freio()
-    string = [a,b]
-    def control(self, 'string'): #Usar um dicionario para executar comandos
+carro = Carro()
+
+string = [a,b]
+    def control(string): #Usar um dicionario para executar comandos
         return{
-            'a':self._freio,
-            'b':2,
+            'a':carro._freio,
+            'b':carro._motor,
         }[string]
